5552f29e4f6b229b4719ca7a7f65346f6e03bbb8
ling108 <2660302108@qq.com>
2020-04-12 15:53:56 +0800

    4/12 0

--- src/main/java/Release.java
+++ src/main/java/Release.java
@@ -1,6 +1,7 @@
 public class Release {
     public static void main(String[] args) {
         System.out.println("r1");
+        System.out.println("dev1");
         System.out.println("r2");
     }
 }
