1d9fab0ccc831786b0a6132a72b74651e9c41cfe
ling108 <2660302108@qq.com>
2020-04-23 22:44:07 +0800

    4/23 d4

--- src/main/java/Dev.java
+++ src/main/java/Dev.java
@@ -20,7 +20,8 @@ public class Dev {
 
         System.out.println(1);
         System.out.println(2);
-    }
+        System.out.println(3);
+}
 
     private static void changeValue(Student a) {
         a.setName("对对对");
